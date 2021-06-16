7b7d253e1292e4345d184112daa6e86e3b00c6ba
herman-xphp <hermanxphp@gmail.com>
2021-06-16 09:57:44 +0800

    restrukture project mengatasi Error CORS ORIGIN install body-parser

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const productRoute = require('./src/routes/products');
 
-app.use(() => {
-  console.log('halo server....');
+app.use(bodyParser.json())  // type JSON
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 })
+app.use('/', productRoute);
+
 app.listen(4000);
